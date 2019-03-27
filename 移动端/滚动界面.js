78b63350cf30f161773ba2cd980b9f2f8cc8ef88
liuning0427 <1196488380@qq.com>
2019-03-27 18:34:14 +0800

    练习

--- 移动端/滚动界面.js
+++ 移动端/滚动界面.js
@@ -3,4 +3,10 @@ $(document).on('swipeUp',function(){
     // console.log(1111);
     $('.page-'+ iNow).attr("class","page-"+ iNow +"moveToTop");
     $('.page-'+(iNow+1)).attr("class","page-"+(iNow+1)+" moveFromBottom"  );
+
+setTimeout(function(){
+    $('.page-'+(iNow+1)).children().removeClass('hide');
+    $('.page-'+iNow).children().addClass('hide');
+    iNow++;
+    },600);
 })

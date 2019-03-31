66a83b31c81cf0e15cded5de1a60ad2f03d3f747
liuning0427 <1196488380@qq.com>
2019-03-31 15:34:59 +0800

    手机聊天界面

--- mobil/index.js
+++ mobil/index.js
@@ -13,11 +13,13 @@ for(var i=0;i<aLi.length;i++){
 var iNow = 1;
 $(document).on("swipeLeft",function(){
     // console.log(111);
-    $('.page-'+iNow).attr("class","page-"+iNow+"moveToLeft");
+    $('.page-'+iNow).attr("class","page-"+iNow+" moveToLeft");
     $('.page-'+(iNow+1)).attr("class","page-"+(iNow+1)+" moveToRight");
     setTimeout(function(){
-        $('.page-'+(iNow+1)).children().removeClass('hide');
         $('.page-'+iNow).children().addClass('hide');
+        $('.page-'+(iNow+1)).children().removeClass('hide');
         iNow++;
     },600);
 })
+
+

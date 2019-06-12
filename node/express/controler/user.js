528ffc585986ddbec0ab3d57438ddb58df74f61a
liuning0427 <1196488380@qq.com>
2019-06-12 18:22:08 +0800

    node

--- node/express/controler/user.js
+++ node/express/controler/user.js
@@ -2,8 +2,8 @@ exports.reg=function(req,res,next){
     res.render("reg.ejs");
 }
 exports.do_reg=function(req,res,next){
-    var name=req.body.username;
-    var pass=req.body.userpass;
-    console.log(name);
+    var name=req.body.uname;
+    var pass=req.body.pass;
+    console.log(name); 
     console.log(pass);
 }
--- node/express/routes/index.js
+++ node/express/routes/index.js
@@ -4,7 +4,7 @@ var User = require("../controler/user.js")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'haha' });
 });
 router.get("/reg",User.reg);
 router.post("/reg",User.do_reg);

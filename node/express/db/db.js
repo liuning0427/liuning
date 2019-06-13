1d6788152139efd33d116fbb016e631ffd2398ae
liuning0427 <1196488380@qq.com>
2019-06-13 22:11:39 +0800

    node

---
+++ node/express/db/db.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host :'localhost',
+    user:'root',
+    password:'',
+    database:'ning'
+})
+
+exports.query=function(sql,params,callback){
+    pool.query(sql,params,function(error,results,fields){
+        if(error) throw error;
+        callback(error,results);
+    })
+}
--- node/express/routes/index.js
+++ node/express/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var User = require("../controler/user.js")
+var User = require("../controller/user.js")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {

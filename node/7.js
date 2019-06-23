c2f8cb65b2e57cf8aea4b71dd571beac4bc1cc26
liuning0427 <1196488380@qq.com>
2019-06-23 20:37:10 +0800

    node

---
+++ node/7.js
@@ -0,0 +1,4 @@
+var formidable=require("formidable");
+var http=require("http");
+var url=require("url");
+
--- node/post.js
+++ node/post.js
@@ -1,8 +1,9 @@
 var http=require("http");
 var url=require("url");
 var fs=require("fs");
+var querystring=require("querystring");
 
-http.crestServer(function(req,res){
+http.createServer(function(req,res){
     var pathname=url.parse(req.url).pathname;
     switch(pathname){
         case '/':
@@ -18,3 +19,24 @@ http.crestServer(function(req,res){
 }).listen(3000);
 
 console.log(123);
+
+function resIndex(res){
+    var pathname=__dirname+"/static/"+url.parse("index.html").pathname;
+    var indexpage=fs.readFileSync(pathname,"utf-8");
+    res.writeHead(200,{"Content":"text/html"});
+    res.end(indexpage);
+}
+
+function resAdd(req,res){
+    var postData="";
+    req.setEncoding("utf8");
+    req.addListener("data",function(post){
+        postData += post;
+    })
+    req.addListener("end",function(){
+        var param=querystring.parse(postData);
+        var str=param.uname;
+        res.writeHead(200,{"Content":"text/html"});
+        res.end(str);
+    })
+}

c968dc5f96724559f02f8183b5951862f5d65f98
liuning0427 <1196488380@qq.com>
2019-06-26 12:43:17 +0800

    vue

---
+++ newapp/src/components/Right.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <el-row :gutter="10">
+  <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+</el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .el-col {
+    border-radius: 4px;
+    height:200px;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+</style>

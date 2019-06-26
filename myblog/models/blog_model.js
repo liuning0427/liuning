2aae1832958e46622d48696400fee0a1aebdcefe
liuning0427 <1196488380@qq.com>
2019-06-26 22:24:55 +0800

    js

--- myblog/models/blog_model.js
+++ myblog/models/blog_model.js
@@ -1,9 +1,5 @@
 var db=require('./db.js');
 
-// exports.sel_all=function(callback){
-//     var sql="select * from t_blogs";
-//     db.query(sql,[],callback);
-// }
 
 exports.sel_id_by_user=function(uid,callback){
     var sql="select * from t_users,t_blogs,t_blog_catalogs where t_users.USER_ID=t_blogs.WRITER and t_blogs.CATALOG_ID=t_blog_catalogs.CATALOG_ID and t_blogs.WRITER=?";
